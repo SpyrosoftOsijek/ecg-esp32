994080a67f816661509ce89a6132c14309482ad5
valentina.jelavic <valentina.jelavic@gmail.com>
2024-07-22 20:51:45 +0200

    Update mtu size

--- main/main.cpp
+++ main/main.cpp
@@ -9,12 +9,12 @@ constexpr std::uint32_t kTaskStackSize{2048};
 
 extern "C" void app_main(void) {
     ESP_LOGD(MAIN_TAG, "Application startup");
-    std::size_t mtu_data_size = 512; 
+    std::size_t mtu_data_size = 62; 
     ECGDataQueue data_queue;
 
     auto uartDataProvider = std::make_unique<ecg::provider::UartECGDataProvider>(CONFIG_EMULATE_UART_GPIO_TX, CONFIG_EMULATE_UART_GPIO_RX);
     ecg::ECG ecg(std::move(uartDataProvider), mtu_data_size); 
     data_queue= ecg.GetECGDataQueue();
-
+   
     ecg.StartGatheringECGData(kTaskStackSize, kTaskUxPriority);
 }
