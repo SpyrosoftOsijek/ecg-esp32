24c65907c359942e159a2698df82162db4f78b5c
valentina.jelavic <valentina.jelavic@gmail.com>
2024-09-02 12:45:40 +0200

    Fix callback for accessing ecg data

--- components/ecg/test/include/mock_ecg_data_provider.h
+++ components/ecg/test/include/mock_ecg_data_provider.h
@@ -1,27 +1,24 @@
-#ifndef ECG_MOCK_ECG_DATA_PROVIDER_H
-#define ECG_MOCK_ECG_DATA_PROVIDER_H
+#ifndef MOCK_ECG_DATA_PROVIDER_H
+#define MOCK_ECG_DATA_PROVIDER_H
 
 #include "iecg_data_provider.h"
-#include <cstddef>
-#include <cstdint>
-#include <queue>
 #include <vector>
+#include <queue>
 
 namespace ecg {
 namespace provider {
 
-class MockECGDataServiceProvider : public IECGDataProvider {
+class MockECGDataProvider : public IECGDataProvider {
 public:
-    MockECGDataServiceProvider() {}
-
+    void AddTestData(const std::uint16_t* data, std::size_t size);
     bool PollECGData() override;
-    void AddTestData(const uint16_t* data, size_t length);
-    void SetUpTestData(std::uint16_t data);
 
 private:
-    void AddECGData(const uint16_t* data, size_t length);
-    std::queue<std::uint16_t> dataset_;
+    std::queue<std::uint16_t> mock_data_;
 };
+
 } // namespace provider
 } // namespace ecg
+
 #endif // MOCK_ECG_DATA_PROVIDER_H
+
--- main/main.cpp
+++ main/main.cpp
@@ -1,30 +1,36 @@
 #include "bluetooth_ecg_wrapper.h"
 #include "esp_log.h"
 #include "freertos/projdefs.h"
-//#include "ecg.h"
-//#include "uart_ecg_data_provider.h"
+#include "ecg.h"
+#include "uart_ecg_data_provider.h"
+#include <memory>
 
-
-constexpr const char* MAIN_TAG = "Main";
+constexpr const char *MAIN_TAG = "Main";
 
 extern "C" void app_main(void)
 {
+
     ESP_LOGD(MAIN_TAG, "Application startup");
-
-    // auto uartDataProvider = std::make_unique<ecg::provider::UartECGDataProvider>(CONFIG_EMULATE_UART_GPIO_TX, CONFIG_EMULATE_UART_GPIO_RX);
-    // ecg::ECG ecg(std::move(uartDataProvider)); 
-
+    std::size_t mtu_data_size = 62;
 
     BluetoothECGWrapper bt_ecg_wrapper{};
-    std::uint16_t my_data{0x1234};
+
+    auto data_received_callback = [&bt_ecg_wrapper](auto data)
+    {
+        ESP_LOGI(MAIN_TAG, "Sending data: %d", data);
+        bt_ecg_wrapper.send_data(data);
+    };
+
+    auto uartDataProvider = std::make_unique<ecg::provider::UartECGDataProvider>(CONFIG_EMULATE_UART_GPIO_TX, CONFIG_EMULATE_UART_GPIO_RX);
+    ecg::ECG ecg(std::move(uartDataProvider), data_received_callback, mtu_data_size);
 
     while (1)
     {
+        vTaskDelay(pdMS_TO_TICKS(500));
         if (bt_ecg_wrapper.isConnected())
         {
-            bt_ecg_wrapper.send_data(++my_data);
+            ecg.PollData();
         }
-        vTaskDelay(pdMS_TO_TICKS(500));
     }
 
 }
