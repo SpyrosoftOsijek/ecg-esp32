29e960f8bf34ec29ce85c4dee8c217503c40bcef
fkuatspyro <123727166+fkuatspyro@users.noreply.github.com>
2024-09-03 09:57:24 +0200

    Parse heartbeat and clean up in bt_ecg_wrapper (#9)

--- components/bt_ecg_wrapper/include/bluetooth_ecg_wrapper.h
+++ components/bt_ecg_wrapper/include/bluetooth_ecg_wrapper.h
@@ -1,26 +1,11 @@
-// Include guards?
-// const, noexcept metode?
+#ifndef BT_ECG_WRAPPER_BLUETOOTH_ECG_WRAPPER_H
+#define BT_ECG_WRAPPER_BLUETOOTH_ECG_WRAPPER_H
 
-#include <array>
-#include <cstdint>
-#include <stdio.h>
-#include <inttypes.h>
-#include "sdkconfig.h"
-#include "esp_log.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "esp_system.h"
 #include "esp_bt.h"
-#include "esp_gap_bt_api.h"
-#include "esp_bt_main.h"
-#include "esp_bt_device.h"
-#include "esp_spp_api.h"
-#include <esp_gap_ble_api.h>
-#include <nvs_flash.h>
-#include <esp_gattc_api.h>
-#include <esp_gatt_common_api.h>
-#include <esp_gatts_api.h>
-#include <string.h>
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+
+#include <cstdint>
 
 class BluetoothECGWrapper
 {
@@ -35,3 +20,5 @@ private:
     static void esp_gatts_cb(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
     static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
 };
+
+#endif // BT_ECG_WRAPPER_BLUETOOTH_ECG_WRAPPER_H
